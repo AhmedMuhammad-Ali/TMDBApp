28ba4221eceaaf8fdbd587f3b6fa73c6dfb91583
Ahmed Ali <ahmedmuhammad.aliamin@gmail.com>
2024-01-30 17:12:36 +0200

    add: movies use case and repositories interfaces

---
+++ Domain/Sources/Domain/Interfaces/Repositories/MoviesRepository.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesRepository.swift
+//
+//
+//  Created by Ahmed Ali on 30/01/2024.
+//
+
+import Foundation
+/// A protocol defining the requirements for fetching movie information.
+public protocol MoviesRepository {
+    /// Fetches information for all users.
+    ///
+    /// - Returns: An array of `Movie` instances representing movie information.
+    ///
+    /// - Throws: An error of type `Error` if the movie information cannot be retrieved.
+    func fetchAllMovies() async throws -> [Movie]
+}
